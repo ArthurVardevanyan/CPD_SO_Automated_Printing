a9abffb88b7d78619e41936fd30f229949097e29
Arthur <21159051+ArthurVardevanyan@users.noreply.github.com>
2019-05-26 20:55:57 -0400

    Optimization

--- BannerSheet.py
+++ BannerSheet.py
@@ -1,9 +1,18 @@
 import json
 
-#Setups up BannerSheet Postscript File
+# Setups up BannerSheet Postscript File
+
+
 def bannerSheet(JobInfo, path):
-    with open('PJL_Commands/BannerSheetRed', 'rb') as f:
+
+    BannerSheetColor = 'green'
+    with open('PJL_Commands/BannerSheet.ps', 'rb') as f:
         bannerlines = f.readlines()
+
+    for i in range(len(bannerlines)):
+        if str('<media-color syntax="keyword">') in str(bannerlines[i]):
+            bannerlines[i] = str.encode(
+                '@PJL XCPT 				<media-color syntax="keyword">' + BannerSheetColor + '</media-color>\n')
 
     filedict = JobInfo.get('Files', False)
     filelist = []
@@ -19,21 +28,25 @@ def bannerSheet(JobInfo, path):
         '/Times findfont 12 scalefont setfont\n',
     ]
     vline = int(600)
-    with open(path+JobInfo.get('Order Number', False) + ' Banner Sheet.ps', 'wb') as outfile:
+    with open(path+JobInfo.get('Order Number', False) + ' Banner ' + JobInfo.get('First Name', False) + ' ' + JobInfo.get('Last Name', False) + '.ps', 'wb') as outfile:
         for lines in bannerlines:
             outfile.write(lines)
 
         for line in PSFILE:
             outfile.write(str.encode(line))
         for key, value in JobInfo.items():
-            if(key != 'Files' and key != 'Ran'):
+            if(key != 'Files' and key != 'Ran'and key != 'Account ID'and key != 'Order Number' and key != 'Building'):
                 outfile.write(str.encode('75 ' + str(vline) +
                                          ' moveto ('+str(key) + ": " + str(value)+' ) show\n'))
                 vline = int(vline) - 17
         vline = int(vline) - 20
         for items in filelist:
+            items = items.split("', '")
             outfile.write(str.encode('25 ' + str(vline) +
-                                     ' moveto ('+str(items) + ' ) show\n'))
+                                     ' moveto ('+str(items[0]) + ' ) show\n'))
+            vline = int(vline) - 17
+            outfile.write(str.encode('25 ' + str(vline) +
+                                     ' moveto ('+str(items[1]) + ' ) show\n'))
             vline = int(vline) - 17
         outfile.write(str.encode('showpage\n'))
     return path+JobInfo.get('Order Number', False) + ' Banner Sheet.ps'
