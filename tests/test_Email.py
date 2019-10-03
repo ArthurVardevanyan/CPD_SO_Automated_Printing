3fc2e9e180c5a8456cd531a74647e277f8043505
Arthur <21159051+ArthurVardevanyan@users.noreply.github.com>
2019-10-03 16:51:20 -0400

    More Email Tests

--- tests/test_Email.py
+++ tests/test_Email.py
@@ -6,8 +6,34 @@ import Email
 
 
 class Testing(unittest.TestCase):
+    def test_subject_line(self):
+        self.assertEqual(Email.subject_line(["OK", [[
+            "(b'121234 (UID 121234 BODY[HEADER.FIELDS (Subject)] {00}'", "b'Subject: First Last - Copy Job - Test Job\r\n\r\n')"], "b')'"]]), "First Last - Test")
+        remove = ("<", ">", "|", ".", ";", ":", "*", "?", "\\", "/", '"')
+        for i in remove:
+            self.assertEqual(Email.subject_line(["OK", [[
+                "(b'121234 (UID 121234 BODY[HEADER.FIELDS (Subject)] {00}'", "b'Subject: First Last - Copy Job - Test "+i+"Job\r\n\r\n')"], "b')'"]]), "First Last - Test")
+
+    def test_order_number_extract(self):
+        with open('tests/SO/11344-2704 First Last - Test 1/11344-2704 First Last - Test 1.txt', 'r') as file:
+            data = file.read()
+        self.assertEqual(Email.order_number_extract(
+            data, "-0351"), ('11344-0351', ""))
+
+    def test_link_extractor(self):
+        with open('Credentials/11349-0311.txt', 'r') as file:
+            data = file.read()
+        email_body = Email.link_extractor(str(data))
+        self.assertEqual(len(email_body), 9)
+
+    def test_link_cleanup(self):
+        with open('Credentials/11349-0311.txt', 'r') as file:
+            data = file.read()
+        email_body = Email.link_extractor(str(data))
+        self.assertEqual(len(Email.link_cleanup(email_body)), 9)
+
     def test_order_number_random(self):
-        self.assertIn("101234-", Email.order_number_random("101234"))
+        self.assertIn("-", Email.order_number_random())
 
     def test_duplex_test(self):
         self.assertEqual(Email.duplex_state(
