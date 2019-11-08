12f9403f29b47006470dbda46d59812606827897
Arthur <21159051+ArthurVardevanyan@users.noreply.github.com>
2019-11-08 12:56:29 -0500

    Page Count Failover

--- files.py
+++ files.py
@@ -1,5 +1,5 @@
 # files.py
-__version__ = "v20191018"
+__version__ = "v20191108"
 
 # Built-In Libraries
 import os
@@ -9,6 +9,14 @@ import glob
 import PyPDF2
 from termcolor import colored
 import colorama
+import subprocess
+
+
+# Local Files
+if(os.name == "posix"):
+    GHOSTSCRIPT_PATH = 'gs'
+else:
+    GHOSTSCRIPT_PATH = 'C:/"Program Files (x86)"/gs/gs9.27/bin/gswin32c.exe'
 
 # use Colorama to make Termcolor work on Windows too
 colorama.init()
@@ -43,12 +51,27 @@ def postscript_list(folder, OName, sub):
 def page_counts(OUTPUT_DIRECTORY, ORDER_NAME):
     # Returns the total page counts for an Order
     files = file_list(OUTPUT_DIRECTORY, ORDER_NAME)
-    page_count = 0
+    counts = 0
     print("\n")
     for i in range(len(files)):
-        pdf = PyPDF2.PdfFileReader(
-            open("".join([OUTPUT_DIRECTORY, '/', ORDER_NAME, '/', files[i]]), "rb"))
-        print("Page Count: ", colored(str(pdf.getNumPages()),
+        try:
+            pdf = PyPDF2.PdfFileReader(
+                open("".join([OUTPUT_DIRECTORY, '/', ORDER_NAME, '/', files[i]]), "rb"))
+            pdf = pdf.getNumPages()
+        except:
+            pdf = page_count(
+                '/'.join([OUTPUT_DIRECTORY, '/', ORDER_NAME, '/', files[i]]))
+        print("Page Count: ", colored(str(pdf),
                                       "magenta"), " FileName: ", files[i])
-        page_count = page_count + pdf.getNumPages()
-    return page_count
+        counts = counts + pdf
+    return counts
+
+
+def page_count(path):
+    args = [GHOSTSCRIPT_PATH, "-q", "-dNODISPLAY", '-c',
+            '"('+path + ') (r) file runpdfbegin pdfpagecount = quit"']
+    status = subprocess.Popen(args, stdout=subprocess.PIPE)
+    (out, err) = status.communicate()  # pylint: disable=unused-variable
+    out = out.strip()
+    out = [int(s) for s in out.split() if s.isdigit()]
+    return out[0]
