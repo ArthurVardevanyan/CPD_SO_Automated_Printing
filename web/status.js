5e3638cc0dff7614e0aaa306ebcb1151aeda3b1f
Arthur <arthur@arthurvardevanyan.com>
2020-02-28 15:42:36 -0500

    Web Order Status Tracker

---
+++ web/status.js
@@ -0,0 +1,61 @@
+window.onload = function () {
+    $.ajax({
+        url: "http://localhost/web/statusL.php",
+        method: "GET",
+        success: function (data) {
+            $(document).ready(function () {
+                //https://datatables.net/forums/discussion/32107/how-to-load-an-array-of-json-objects-to-datatables
+                var aDemoItems = data
+
+                //Load  data table
+                var oTblReport = $("#LeftToPrint")
+
+                oTblReport.DataTable({
+                    "pageLength": 25,
+                    data: aDemoItems,
+                    "order": [[0, "desc"]],
+
+                    "columns": [
+                        {
+                            "data": "order_number", "title": "Order Number",
+                            "render": function (data, type, row, meta) {
+                                if (type === 'display') {
+                                    data = '<a href=/web/order.php?id=' + aDemoItems[0].email_id + '&on=' + data + '>' + data + '</a>';
+                                }
+
+                                return data;
+                            }
+                        },
+                        { "data": "order_subject", "title": "Order Subject" },
+                        { "data": "sheets", "title": "Sheets" },
+
+                    ],
+
+                });
+            });
+        }
+    })
+    $.ajax({
+        url: "http://localhost/web/statusG.php",
+        method: "GET",
+        success: function (data) {
+            $(document).ready(function () {
+                //https://datatables.net/forums/discussion/32107/how-to-load-an-array-of-json-objects-to-datatables
+                var aDemoItems = data
+
+                //Load  data table
+                var oTblReport = $("#LeftToPrintSum")
+
+                oTblReport.DataTable({
+                    "pageLength": 25,
+                    data: aDemoItems,
+                    "columns": [
+                        { "data": "name", "title": "Deliver To:" },
+                        { "data": "sheets", "title": "Sheets" },
+
+                    ]
+                });
+            });
+        }
+    })
+};
