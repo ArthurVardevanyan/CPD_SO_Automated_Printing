95d8f1b16f13aead10350d56ea43b00d7cbf977d
Arthur <arthur@arthurvardevanyan.com>
2020-03-24 16:27:24 -0400

    Function Refactor

--- database.py
+++ database.py
@@ -1,5 +1,4 @@
-__version__ = "v20200302"
-
+__version__ = "v20200324"
 import mysql.connector
 import files
 import json
@@ -8,24 +7,13 @@ import order as o
 import instructions
 
 
-def database_input(OUTPUT_DIRECTORY, JOB_INFO):
-    order = o.Order()
-
-    try:
-        order.OD = OUTPUT_DIRECTORY
-        order.NAME = JOB_INFO.get('Order Number', "0") + \
-            " " + JOB_INFO.get('Order Subject', "0")
-        order = o.order_initialization(order, JOB_INFO)
-    except:
-        print("Order Initialization Failure")
-
+def credentials():
     try:
         with open("Credentials/db.txt") as f:
             cred = f.readlines()
         cred = [x.strip() for x in cred]
     except:
         print("Credential Failure")
-
     db = mysql.connector.connect(
         host="localhost",
         user=cred[0],
@@ -33,9 +21,20 @@ def database_input(OUTPUT_DIRECTORY, JOB_INFO):
         database='school_orders',
         auth_plugin='mysql_native_password'
     )
+    return db
 
+
+def database_input(OUTPUT_DIRECTORY, JOB_INFO):
+    order = o.Order()
+    try:
+        order.OD = OUTPUT_DIRECTORY
+        order.NAME = JOB_INFO.get('Order Number', "0") + \
+            " " + JOB_INFO.get('Order Subject', "0")
+        order = o.order_initialization(order, JOB_INFO)
+    except:
+        print("Order Initialization Failure")
+    db = credentials()
     cursor = db.cursor()
-
     add_teacher = ("INSERT IGNORE INTO teachers "
                    "(email,first_name,last_name,phone) "
                    "VALUES (%s, %s, %s, %s)")
@@ -45,7 +44,6 @@ def database_input(OUTPUT_DIRECTORY, JOB_INFO):
         JOB_INFO.get('Last Name', "0"),
         JOB_INFO.get('Phone Number', "0"),
     )
-
     add_order = ("INSERT IGNORE INTO order_data"
                  "( email_id,order_number,order_subject,date_ordered,status,cost,email,copies,duplex,collation,paper,stapling,\
                         drilling,folding,cutting,booklets,front_cover,back_cover,special_instructions,slip_shrink,sheets) "
@@ -74,7 +72,6 @@ def database_input(OUTPUT_DIRECTORY, JOB_INFO):
         JOB_INFO.get('Slip Sheets / Shrink Wrap', "None"),
         order.PAGE_COUNTS * order.COPIES / instructions.duplex_state(order),
     )
-
     add_deliver = ("INSERT IGNORE INTO deliver "
                    "(order_number,name,address) "
                    "VALUES (%s, %s, %s)")
@@ -82,18 +79,14 @@ def database_input(OUTPUT_DIRECTORY, JOB_INFO):
         JOB_INFO.get('Order Number', "0"),
         JOB_INFO.get('Deliver To Name', "None"),
         JOB_INFO.get('Deliver To Address', "None"),
-
     )
-
     cursor.execute(add_teacher, data_teacher)
     cursor.execute(add_order, data_order)
     cursor.execute(add_deliver, data_deliver)
-
     add_files = ("INSERT IGNORE INTO files "
                  "(file_number, order_number,name,pages) "
                  "VALUES (%s, %s, %s, %s)")
     JOB_INFO_FILES = JOB_INFO.get('Files', False)
-
     for FILES in JOB_INFO_FILES:
         FILE_INFO = JOB_INFO_FILES.get(str(FILES), 0)
         data_files = (
@@ -105,152 +98,50 @@ def database_input(OUTPUT_DIRECTORY, JOB_INFO):
             FILE_INFO.get('Page Count', 0)
         )
         cursor.execute(add_files, data_files)
-
     db.commit()
-
-    db.close
-    return 1
-
-
-def status_change(order):
-
-    try:
-        with open("Credentials/db.txt") as f:
-            cred = f.readlines()
-        cred = [x.strip() for x in cred]
-    except:
-        print("Credential Failure")
-
-    db = mysql.connector.connect(
-        host="localhost",
-        user=cred[0],
-        passwd=cred[1],
-        database='school_orders',
-        auth_plugin='mysql_native_password'
-    )
-
-    cursor = db.cursor()
-    status = "UPDATE order_data SET status = '"+order.status + \
-        "' WHERE order_number = '"+order.NUMBER+"'"
-
-    cursor.execute(status)
-
-    db.commit()
-
     db.close
     return 1
 
 
 def print_status(order, status):
-
-    try:
-        with open("Credentials/db.txt") as f:
-            cred = f.readlines()
-        cred = [x.strip() for x in cred]
-    except:
-        print("Credential Failure")
-
-    db = mysql.connector.connect(
-        host="localhost",
-        user=cred[0],
-        passwd=cred[1],
-        database='school_orders',
-        auth_plugin='mysql_native_password'
-    )
-
+    db = credentials()
     cursor = db.cursor()
     status = "UPDATE order_data SET status = '"+status + \
         "' WHERE order_number = '"+order+"'"
-
     cursor.execute(status)
-
     db.commit()
-
     db.close
     return 1
 
 
-def status_change_all():
-    try:
-        with open("Credentials/db.txt") as f:
-            cred = f.readlines()
-        cred = [x.strip() for x in cred]
-    except:
-        print("Credential Failure")
-
-    db = mysql.connector.connect(
-        host="localhost",
-        user=cred[0],
-        passwd=cred[1],
-        database='school_orders',
-        auth_plugin='mysql_native_password'
-    )
-
+def manual_status_change(orders="NotStarted"):
+    db = credentials()
     cursor = db.cursor()
-    status = "UPDATE order_data SET status = 'Printed' WHERE status  = 'NotStarted'"
-
-    cursor.execute(status)
-
-    db.commit()
-
-    db.close
-    return 1
-
-
-def status_change_some(orders):
-    try:
-        with open("Credentials/db.txt") as f:
-            cred = f.readlines()
-        cred = [x.strip() for x in cred]
-    except:
-        print("Credential Failure")
-
-    db = mysql.connector.connect(
-        host="localhost",
-        user=cred[0],
-        passwd=cred[1],
-        database='school_orders',
-        auth_plugin='mysql_native_password'
-    )
-
-    cursor = db.cursor()
-    for order in orders:
-        status = "UPDATE order_data SET status = 'Printed' WHERE `order_number`  like '%" + order + "%'"
+    if orders == "NotStarted":
+        status = "UPDATE order_data SET status = 'Printed' WHERE status  = 'NotStarted'"
         cursor.execute(status)
         db.commit()
-
+    else:
+        for order in orders:
+            status = "UPDATE order_data SET status = 'Printed' WHERE `order_number`  like '%" + order + "%'"
+            cursor.execute(status)
+            db.commit()
     db.close
     return 1
 
 
 def printingOrders():
-    try:
-        with open("Credentials/db.txt") as f:
-            cred = f.readlines()
-        cred = [x.strip() for x in cred]
-    except:
-        print("Credential Failure")
-
-    db = mysql.connector.connect(
-        host="localhost",
-        user=cred[0],
-        passwd=cred[1],
-        database='school_orders',
-        auth_plugin='mysql_native_password'
-    )
-
+    db = credentials()
     cursor = db.cursor()
-    query = ("SELECT `order_number`, `status` FROM `order_data` WHERE `status` LIKE '%%P1%%'")
+    query = (
+        "SELECT `order_number`, `status` FROM `order_data` WHERE `status` LIKE '%%P1%%'")
     cursor.execute(query)
-
     orders = []
-    for (order_number , status) in cursor:
+    for (order_number, status) in cursor:
         order = (str(order_number).replace("(", "").replace(
             ")", "").replace(",", "").replace("'", ""), str(status).replace("(", "").replace(
             ")", "").replace(",", "").replace("'", "").replace("P", ""))
-            
         orders.append(order)
-
     db.close
     return orders
 
@@ -265,7 +156,6 @@ def main(OUTPUT_DIRECTORY):
         folders = files.folder_list(OUTPUT_DIRECTORY)
         ORDER_NAMES = []
         for ORDER_NUMBER in range(int(Start), int(End)+1):
-
             ORDER_NUMBER = str(ORDER_NUMBER).zfill(5)
             for i in folders:  # Searchs for Requested Order Number from list of currently downloaded orders
                 if ORDER_NUMBER in i:
@@ -282,9 +172,9 @@ def main(OUTPUT_DIRECTORY):
         while(temp != "run"):
             temp = str(input("Type In an Order Number: "))
             ORDER_NUMBER.append(temp)
-        status_change_some(ORDER_NUMBER)
+        manual_status_change(ORDER_NUMBER)
     elif(int(option) == 3):
-        status_change_all()
+        manual_status_change()
 
 
 if __name__ == "__main__":
