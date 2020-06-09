b4dfe2fbc68402e1663bb017e5ce1a5e266dc9cd
Arthur <21159051+ArthurVardevanyan@users.noreply.github.com>
2020-06-09 11:45:37 -0400

    executable dependancy removal

--- instructions.py
+++ instructions.py
@@ -1,5 +1,7 @@
-__version__ = "v20200304"
+__version__ = "v20200609"
 import PostScript
+from PJL_Commands.PJL_PS import end
+from PJL_Commands.PJL_PS import start
 import log
 import re
 import os
@@ -292,13 +294,15 @@ def pjl_merge(order, outFOLDER, MERGED, COVERS, FILES):
     if COVERS == True:
         # Add the PJL Commands to the merged file in preperation to print.
         for i in range(len(FILES)):
-            file_names = ['PJL_Commands/input.ps',  'PJL_Commands/FrontCover.ps', order.OD+"/"+order.NAME +
-                          "/PostScript"+N+"/"+FILES[i]+".ps", 'PJL_Commands/End.ps']
+            file_names = ['input.ps',  'PJL_Commands/FrontCover.ps', order.OD+"/"+order.NAME +
+                          "/PostScript"+N+"/"+FILES[i]+".ps", ]
             with open(order.OD+"/"+order.NAME + "/" + outFOLDER + "/"+FILES[i][:40][:-4]+".ps", 'wb') as outfile:
                 for fname in file_names:
                     with open(fname, 'rb') as infile:
                         if fname == file_names[0] or fname == file_names[1] or fname == file_names[len(file_names)-1]:
                             for line in infile:
+                                outfile.write(line)
+                            for line in end.splitlines():
                                 outfile.write(line)
                         else:
                             BeginProlog = False
@@ -310,23 +314,27 @@ def pjl_merge(order, outFOLDER, MERGED, COVERS, FILES):
         return 1
     elif MERGED == True:
         # Add the PJL Commands to the merged file in preperation to print.
-        file_names = ['PJL_Commands/input.ps', order.OD+"/" +
-                      order.NAME + "/"+order.NAME+N+".ps", 'PJL_Commands/End.ps']
+        file_names = ['input.ps', order.OD+"/" +
+                      order.NAME + "/"+order.NAME+N+".ps", ]
         with open(order.OD+"/"+order.NAME + "/"+outFOLDER + "/"+order.NAME+".ps", 'wb') as outfile:
             for fname in file_names:
                 with open(fname, 'rb') as infile:
                     for line in infile:
                         outfile.write(line)
+                    for line in end.splitlines():
+                        outfile.write(line)
         return 1
     elif MERGED == False:
         # Add the PJL Commands to the files in preperation to print.
         for i in range(len(FILES)):
-            file_names = ['PJL_Commands/input.ps', order.OD+"/"+order.NAME +
-                          "/PostScript"+N+"/"+FILES[i]+".ps", 'PJL_Commands/End.ps']
+            file_names = ['input.ps', order.OD+"/"+order.NAME +
+                          "/PostScript"+N+"/"+FILES[i]+".ps"]
             with open(order.OD+"/"+order.NAME + "/" + outFOLDER + "/"+FILES[i][:40][:-4]+".ps", 'wb') as outfile:
                 for fname in file_names:
                     with open(fname, 'rb') as infile:
                         for line in infile:
+                            outfile.write(line)
+                        for line in end.splitlines():
                             outfile.write(line)
         return 1
     return 0
@@ -346,8 +354,7 @@ def pjl_insert(order, COPIES_PER_SET, COVERS):
     size = size_extract(order)
     COPIES_COMMAND = str.encode("".join(
         ['@PJL XCPT <copies syntax="integer">', str(COPIES_PER_SET), '</copies>\n', COVER]))
-    with open('PJL_Commands/PJL.ps', 'rb') as f:
-        lines = f.readlines()
+    lines = start.splitlines()
     # Modifies the PJL file before adding it to the postscript files
     for i in range(len(lines)):
         if str('<media-color syntax="keyword">') in str(lines[i]):
@@ -391,7 +398,7 @@ def pjl_insert(order, COPIES_PER_SET, COVERS):
             lines[i] = str.encode(
                 '@PJL XCPT 		<output-bin syntax="keyword">automatic</output-bin>\n')
     # The Postscript/PJL commands file that gets inserted before the file.
-    with open('PJL_Commands/input.ps', 'wb') as f:
+    with open('input.ps', 'wb') as f:
         for item in lines:
             f.write(item)
     # If it makes sense to use merged files, it uses them.
